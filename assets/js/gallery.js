1999fc0fbacf488b13c15ea53dd6d10e1b1d580a
Ubuntu <ubuntu@ip-172-31-34-66.ec2.internal>
2019-08-06 14:12:58 +0000

    Fixed carousel images in header

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,17 +1,35 @@
 const panels = document.querySelectorAll('.panel');
-  
-  function toggleOpen() {
-      console.log('widen photo');
-      this.classList.toggle('open');
+
+function toggleOpen() {
+  console.log('widen photo');
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
   }
-  
-  function toggleActive(e) {
-     console.log(e.propertyName);
-     if(e.propertyName.includes('flex')) {
-        this.classList.toggle('open-active');    
-     }
-     
+
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
-  
-  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-  panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+  myIndex++;
+  if (myIndex > x.length) { myIndex = 1 }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3500); // Change image every 3.5 seconds
+}
+
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
